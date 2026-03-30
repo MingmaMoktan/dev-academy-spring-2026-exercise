5255359374e380c2076428610689a31e06953dbe
sb13512 <sb13512@student.samk.fi>
2026-03-30 21:51:39 +0300

    moved the data inside the docker in database and checked the data

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import requests
+
+API_URL = "http://fastapi-backend:8000"
 
 # Set page title
 st.set_page_config(page_title="Solita Electricity Data")
@@ -7,6 +10,6 @@ st.title("Solita Electricity Dashboard")
 st.write("Frontend is running! Now we just need to fetch some data from the backend.")
 
 # Simple test button
-if st.button('Check Backend Connection'):
-    st.write("Checking...")
-    # Later we will add: requests.get("http://backend:8000/")
+def get_data():
+    response = requests.get(f"{API_URL}/")
+    return response.json()
